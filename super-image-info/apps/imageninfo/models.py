04ee252e5e9bdb8f1536c204a6dd3aa52022f287
JEnriquePS <jenrique.ps@gmail.com>
2015-03-31 10:46:54 -0500

    update models

--- super-image-info/apps/imageninfo/models.py
+++ super-image-info/apps/imageninfo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Imagen(models.Model):
+    imagen = models.ImageField(upload_to="imagen")
+
+    class Meta:
+        verbose_name = "Imagen"
+        verbose_name_plural = "Imagens"
+
+    # def __unicode__(self):
+    #     return self.imagen
+
+
